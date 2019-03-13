925b3a5ac2e9b70f4df8c78b921077cea977ee4b
Didier Matos <d.matostorres@gmail.com>
2019-03-13 15:33:46 -0500

    Realizando la conexión con la API de DarkSky

--- app/src/main/java/com/ndevelopers/stormy/MainActivity.java
+++ app/src/main/java/com/ndevelopers/stormy/MainActivity.java
@@ -2,14 +2,47 @@ package com.ndevelopers.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String apiKey = "683c6948e70da33639b4750934accc0d";
+
+        double lattitude = 37.8267;
+        double longitude = -122.4233;
+
+        String forecastURL = "https://api.darksky.net/forecast/" + apiKey + "/" + lattitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+
+        Call call = client.newCall(request);
+
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()){
+                Log.v(TAG,response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "IO Exception Caught: ", e);
+        }
 
 
     }
